1c184e630b0dbd58a52243321e00554b12ee5796
Srijavali <kothasrijavali@gmail.com>
2026-08-09 00:37:53 +0530

    chore: add initial database migration

---
+++ backend/persistence/migrations/env.py
@@ -0,0 +1,77 @@
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+
+from backend.configuration.settings import settings
+from backend.persistence.database import Base
+from backend.persistence import models  # noqa: F401
+
+
+# Alembic Config object
+config = context.config
+
+
+# Configure Python logging from alembic.ini
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+# Importing backend.persistence.models above registers all SQLAlchemy
+# models with Base.metadata.
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in offline mode."""
+
+    url = settings.DATABASE_URL
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection) -> None:
+    """Configure Alembic using an active database connection."""
+
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in online mode using AsyncEngine."""
+
+    configuration = config.get_section(config.config_ini_section)
+
+    connectable = async_engine_from_config(
+        configuration,
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+        url=settings.DATABASE_URL,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    import asyncio
+
+    asyncio.run(run_migrations_online())
